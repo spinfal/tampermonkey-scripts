178d9a70cb2fd758cad57d079669402706ef3275
spinfal <spinfal@atheist.com>
2021-01-08 10:22:43 -0800

    added mathpapa-nopro
    Allows you to use MathPapa's calculator and show the steps for work without needing to buy the Pro version

---
+++ mathpapa-nopro.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         MathPapa No-Pro Calculator 
+// @namespace    http://waa.ai/spinpy
+// @version      1.3
+// @description  Allows you to use MathPapa's calculator and show the steps for work without needing to buy the Pro version
+// @author       Spinfal
+// @match        https://mathpapa.com/*
+// @grant        none
+// ==/UserScript==
+ 
+(function() {
+    'use strict';
+ 
+    document.getElementById("parse_btn").innerText = 'Go! (No-Pro)';
+    document.getElementById("myaid").style = 'display: none;'; // hides the ad, cuz why not
+ 
+    function noPro() {
+        var today = new Date();
+        var month = today.getMonth();
+        month = month + 1;
+        var day = today.getDate();
+        var year = today.getFullYear();
+        var date = month + '/' + day + '/' + year;
+        console.log(date);
+        localStorage.removeItem('count.t-steps.total');
+        localStorage.removeItem('count.p-steps.' + date);
+    }
+ 
+    document.getElementById("parse_btn").addEventListener("click", noPro);
+    document.getElementById("soloutbuttondiv").addEventListener("click", noPro);
+    document.getElementById("solstepshowlink").addEventListener("click", noPro);
+    document.getElementById("solpickdiv").addEventListener("click", noPro);
+})();
