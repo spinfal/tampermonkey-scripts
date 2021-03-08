a349acf45bae42b935de9a10302c026dc68a0a0b
spin~ <spinfal@atheist.com>
2021-03-07 22:33:19 -0800

    Create chihuahuaspin.user.js

---
+++ chihuahuaspin.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Chihuahuaspin Cheat
+// @namespace    https://waa.ai/spinpy
+// @version      1.0
+// @description  Allows you to change the amount of spins in chihuahuaspin.com
+// @author       Spinfal
+// @match        http://www.chihuahuaspin.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function change(amount) {
+        if (isNaN(amount)) {
+            alert('that wasnt a number.');
+            window.location.reload();
+        } else {
+            document.getElementsByTagName("img")[0].src = 'http://cdn.funnyisms.com/d3540090-1765-4633-99ff-1bb3ba7e40ec.gif';
+            $(window).load(function (){
+                var spins = amount;
+                setInterval(function () {
+                    spins = spins + 1;
+                    $("#spin").text(spins + " spin" + (spins != 1 ? "s" : ""));
+                    if (spins == 10)
+                        $("#ethan").show();
+                    else if (spins == 17)
+                        $("#ethan").hide();
+                }, 4205);
+            });
+        }
+    }
+
+    var set = prompt('amount of spins');
+    if (set === null) {
+        change(0);
+    } else {
+        change(parseInt(set));
+    }
+
+})();
