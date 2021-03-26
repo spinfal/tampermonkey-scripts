1bd10a9798440b0eeee9fd1da5cc30cc11becdcf
spin~ <spinfal@atheist.com>
2021-03-26 16:01:03 -0700

    Update cookieclicker.user.js

--- cookieclicker.user.js
+++ cookieclicker.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Auto Cookie Clicker
 // @namespace    https://waa.ai/spinpy
-// @version      1.0
-// @description  Autoclicks the Cookie and gets you easy Cookies!
+// @version      1.1
+// @description  Autoclicks the Cookie and gets you easy Cookies and upgrades!
 // @author       Spinfal
 // @match        http://orteil.dashnet.org/cookieclicker/
 // @grant        none
@@ -13,9 +13,20 @@
 
     function click() {
         setInterval(function() { document.getElementById('bigCookie').click() }, 0);
+
     }
 
-    if (confirm('start autoclicker?')){
+    function upgrades() {
+        let delay = prompt('enter an interval to auto buy upgrades (in seconds)');
+        console.log(`delay set to: ${delay}`);
+        setInterval(function() { document.getElementsByClassName('crate upgrade')[0].click() }, parseInt(delay) * 1000);
+    }
+
+    if (confirm('start autoclicker?')) {
         click();
+    }
+
+    if (confirm('auto buy upgrades?')) {
+        upgrades();
     }
 })();
