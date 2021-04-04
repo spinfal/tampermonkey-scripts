9070d2107c9f0cea9282a1f680e3622bd755ce68
spin~ <spinfal@atheist.com>
2021-04-03 18:27:21 -0700

    Create activity.user.js

---
+++ experiments/activity.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Discord Activity Enabler
+// @namespace    https://waa.ai/spinpy
+// @version      1.0
+// @description  An easy way to enable Discord VC Activities to use with friends!
+// @author       Spinfal
+// @match        https://discord.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // heh, exeriment. nice one discord
+    localStorage.setItem('exerimentOverrides', '{"2020-11_poker_night":{"type":"guild","revision":1,"bucket":2,"override":true}}');
+})();
