1f46ff4e15983a7225e2e0a3d3ee370e5d0380d5
spin~ <spin@hectic.email>
2021-05-06 00:27:40 -0700

    Update activity.user.js

--- experiments/activity.user.js
+++ experiments/activity.user.js
@@ -2,6 +2,7 @@
 // @name         Discord Activity Enabler
 // @namespace    https://waa.ai/spinpy
 // @version      1.0
+// @run-at       document-start
 // @description  An easy way to enable Discord VC Activities to use with friends!
 // @author       Spinfal
 // @match        https://discord.com/*
@@ -11,7 +12,7 @@
 (function() {
     'use strict';
     
-    // MAKE SURE IT RUNS AT document-start IN SCRIPT SETTINGS!!
+    // thanks andyvuj24 for telling me abt @run-at 
 
     // heh, exeriment. nice one discord
     localStorage.setItem('exerimentOverrides', '{"2020-11_poker_night":{"type":"guild","revision":1,"bucket":2,"override":true}}');
